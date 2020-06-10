6364d1fae916b860bf42d5bb7dbb1dd96a7b13f2
Biswajit Chatterjee <biswajit@sigaramtech.com>
2020-06-10 10:36:27 +0530

    Live chat application

---
+++ frontend/src/components/InfoBar/InfoBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+import closeIcon from '../../icons/closeIcon.png';
+
+import './InfoBar.css';
+
+const InfoBar = ({ name, room }) => (
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+
+      <h3>{name}</h3>
+
+
+    </div>
+    <div className="rightInnerContainer">
+
+      <a href="/"><img src={closeIcon} alt="close icon" /></a>
+    </div>
+  </div>
+);
+
+export default InfoBar;
